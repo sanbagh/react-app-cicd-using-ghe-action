b0d462c573284e74e216c88eeb697b09da1a635e
Sanjeev.Baghel@coxautoinc.com <Sanjeev.Baghel@coxautoinc.com>
2022-09-28 00:35:42 +0530

    feat: some feature

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/sanbagh/react-app-cicd-using-ghe-action",
+    plugins: [
+      "@semantic-release/commit-analyze",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/github"
+    ]
+};
