1aa7ace6f3ab3f2be8d5e9326f24c93034864754
Sanjeev.Baghel@coxautoinc.com <Sanjeev.Baghel@coxautoinc.com>
2022-09-28 00:40:40 +0530

    fix formating

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/sanbagh/react-app-cicd-using-ghe-action",
-    plugins: [
-      "@semantic-release/commit-analyze",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/github"
-    ]
+  branches: "main",
+  repositoryUrl: "https://github.com/sanbagh/react-app-cicd-using-ghe-action",
+  plugins: [
+    "@semantic-release/commit-analyze",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
 };
