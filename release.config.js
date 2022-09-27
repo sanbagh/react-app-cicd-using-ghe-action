2a2044e4dffa0528cb14671b111a27ce57bfb294
Sanjeev.Baghel@coxautoinc.com <Sanjeev.Baghel@coxautoinc.com>
2022-09-28 01:13:23 +0530

    correct misspelled

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: "main",
   repositoryUrl: "https://github.com/sanbagh/react-app-cicd-using-ghe-action",
   plugins: [
-    "@semantic-release/commit-analyze",
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
   ],
